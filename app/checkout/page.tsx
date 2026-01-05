20b91db5ee8314b030947c5a0ad5de861934c3da
Noé Ramos <noecramos@gmail.com>
2026-01-05 00:24:16 -0300

    Fix header banner distortion - use bg-cover instead of bg-[length:100%_100%]

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -87,7 +87,7 @@ export default function CheckoutPage() {
     return (
         <div className="min-h-screen bg-[#F5F5F7] flex flex-col">
             {/* Header Banner */}
-            <div className="h-48 md:h-64 w-full bg-[length:100%_100%] bg-center relative" style={{ backgroundImage: "url('https://i.imgur.com/s2H2qZE.png')" }}>
+            <div className="h-48 md:h-64 w-full bg-cover bg-center relative" style={{ backgroundImage: "url('https://i.imgur.com/s2H2qZE.png')" }}>
                 <div className="absolute inset-0 bg-gradient-to-b from-black/20 to-transparent" />
             </div>
 
