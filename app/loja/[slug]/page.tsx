fb6b691461eab5213032d2576dc03409454090f6
Noé Ramos <noecramos@gmail.com>
2026-01-14 21:33:50 -0300

    revert: Reverting unified header changes that caused UI issues

--- app/loja/[slug]/page.tsx
+++ app/loja/[slug]/page.tsx
@@ -7,7 +7,6 @@ import RestaurantHeader from "../../components/RestaurantHeader";
 import CategoryNav from "../../components/CategoryNav";
 import ProductCard from "../../components/ProductCard";
 import FloatingCart from "../../components/FloatingCart";
-import MainHeader from "../../components/MainHeader";
 
 export const dynamic = 'force-dynamic';
 
@@ -63,7 +62,7 @@ export default function StoreFront() {
 
     return (
         <main style={{ paddingBottom: "100px", minHeight: "100vh" }}>
-            <MainHeader />
+
             {/* Pass dynamic data to header if needed, for now resizing to match */}
             <RestaurantHeader
                 name={restaurant.name}
